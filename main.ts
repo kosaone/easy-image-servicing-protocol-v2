c28a82dc506054a1655e1ad7a7d276cab73e715e
Adam Zaoral <79451876+kosaone@users.noreply.github.com>
2022-01-10 15:13:14 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,54 @@
+input.onGesture(Gesture.TiltRight, function () {
+    if (Stav == 1) {
+        Sprite.change(LedSpriteProperty.X, 1)
+    }
+})
+input.onGesture(Gesture.LogoDown, function () {
+    if (Stav == 1) {
+        Sprite.change(LedSpriteProperty.Y, -1)
+    }
+})
+function OdesilaciMod () {
+	
+}
 input.onButtonPressed(Button.A, function () {
-    if (true) {
-    	
+    PrijmaciMod()
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    if (Stav == 1) {
+        Sprite.change(LedSpriteProperty.X, -1)
+    }
+})
+input.onGesture(Gesture.LogoUp, function () {
+    if (Stav == 1) {
+        Sprite.change(LedSpriteProperty.Y, 1)
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    if (true) {
-    	
-    }
+    Zpet()
 })
 input.onButtonPressed(Button.B, function () {
-	
+    OdesilaciMod()
 })
+function Zpet () {
+    if (Stav == 0) {
+        Stav = 0
+    } else if (Stav == 1) {
+        Stav = 0
+    } else if (Stav == 2) {
+        Stav = 0
+    }
+}
+function PrijmaciMod () {
+    if (Stav == 0) {
+        Stav = 1
+    } else if (Stav == 1) {
+        Sprite = game.createSprite(2, 2)
+    }
+}
+let Sprite: game.LedSprite = null
 let Stav = 0
+Stav = 0
 basic.forever(function () {
 	
 })
