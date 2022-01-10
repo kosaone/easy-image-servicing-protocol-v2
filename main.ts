6dbd164da8d35c3336b0b70bfe18c603e9c1e756
Adam Zaoral <79451876+kosaone@users.noreply.github.com>
2022-01-10 15:01:46 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+    	
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (true) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let Stav = 0
+basic.forever(function () {
+	
+})
