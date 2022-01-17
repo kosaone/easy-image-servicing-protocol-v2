d095ea45a0ae630c160c618aa2d3e63416543043
Adam Zaoral <79451876+kosaone@users.noreply.github.com>
2022-01-17 10:45:35 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,56 @@
 input.onGesture(Gesture.TiltRight, function () {
     if (Stav == 1) {
         Sprite.change(LedSpriteProperty.X, 1)
+        SpriteCurrentX = Sprite.get(LedSpriteProperty.X)
     }
 })
 input.onGesture(Gesture.LogoDown, function () {
     if (Stav == 1) {
         Sprite.change(LedSpriteProperty.Y, -1)
+        SpriteCurrentY = Sprite.get(LedSpriteProperty.Y)
     }
 })
 function OdesilaciMod () {
+    if (Stav == 0) {
+        Stav = 1
+        Sprite = game.createSprite(0, 0)
+    } else if (Stav == 1) {
+        X_Cache = Sprite.get(LedSpriteProperty.X)
+        Y_Cache = Sprite.get(LedSpriteProperty.Y)
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    OdesilaciMod()
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    if (Stav == 1) {
+        Sprite.change(LedSpriteProperty.X, -1)
+        SpriteCurrentX = Sprite.get(LedSpriteProperty.X)
+    }
+})
+input.onGesture(Gesture.LogoUp, function () {
+    if (Stav == 1) {
+        Sprite.change(LedSpriteProperty.Y, 1)
+        SpriteCurrentY = Sprite.get(LedSpriteProperty.Y)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    Zpet()
+})
+input.onButtonPressed(Button.B, function () {
+    PrijmaciMod()
+})
+function Zpet () {
+    if (Stav == 0) {
+        Stav = 0
+        Sprite.delete()
+    } else if (Stav == 1) {
+        Sprite.move(1)
+    } else if (Stav == 2) {
+        Stav = 0
+    }
+}
+function PrijmaciMod () {
     if (Stav == 0) {
         Stav = 2
     } else if (Stav == 1) {
@@ -23,46 +65,22 @@ function OdesilaciMod () {
             `)
     }
 }
-input.onButtonPressed(Button.A, function () {
-    PrijmaciMod()
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    if (Stav == 1) {
-        Sprite.change(LedSpriteProperty.X, -1)
-    }
-})
-input.onGesture(Gesture.LogoUp, function () {
-    if (Stav == 1) {
-        Sprite.change(LedSpriteProperty.Y, 1)
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    Zpet()
-})
-input.onButtonPressed(Button.B, function () {
-    OdesilaciMod()
-})
-function Zpet () {
-    if (Stav == 0) {
-        Stav = 0
-        Sprite.delete()
-    } else if (Stav == 1) {
-        Stav = 0
-        Sprite.delete()
-    } else if (Stav == 2) {
-        Stav = 0
-    }
-}
-function PrijmaciMod () {
-    if (Stav == 0) {
-        Stav = 1
-    } else if (Stav == 1) {
-        Sprite = game.createSprite(2, 2)
-    }
-}
+let Y_Cache = 0
+let X_Cache = 0
+let SpriteCurrentY = 0
+let SpriteCurrentX = 0
 let Sprite: game.LedSprite = null
 let Stav = 0
 Stav = 0
+basic.forever(function () {
+    if ((X_Cache && Y_Cache) != 50) {
+        if ((X_Cache && Y_Cache) != (SpriteCurrentX && SpriteCurrentY)) {
+            led.plot(X_Cache, Y_Cache)
+            X_Cache = 50
+            Y_Cache = 50
+        }
+    }
+})
 basic.forever(function () {
 	
 })
